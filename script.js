f9e5a9ad62331ab7f63b473128de317d3a6366fe
DaleWillemse <DaleWillemse000@gmail.com>
2022-08-08 20:12:44 +0200

    Added space to jump. Need to do hit detection.

--- script.js
+++ script.js
@@ -1,10 +1,16 @@
-let player = document.getElementsByClassName("player");
-let obstacle = document.getElementsByClassName("obstacle");
+var player = document.getElementById("player");
+var obstacle = document.getElementById("obstacle");
 
 function HitDetected(){
     
 }
 
 function JumpPressed(){
-    
+    if (player.classList != "animate"){
+        player.classList.add("animate");
+    }
+    setTimeout(function(){
+        player.classList.remove("animate");
+    },700);
+        
 }
