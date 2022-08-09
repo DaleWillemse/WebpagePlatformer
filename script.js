54fd6a17ee0d1a754bb3c22ba423001d9b049480
DaleWillemse <DaleWillemse000@gmail.com>
2022-08-09 18:27:46 +0200

    Work on hit detection

--- script.js
+++ script.js
@@ -1,6 +1,17 @@
 var player = document.getElementById("player");
 var obstacle = document.getElementById("obstacle");
 
+var obstacleHit = setInterval(function(){
+    var playerTop = parseInt(window.getComputedStyle(player).getPropertyValue("top"));
+    var obstacleLeft = parseInt(window.getComputedStyle(obstacle).getPropertyValue("left"));
+
+    if (obstacleLeft < 151 && obstacleLeft > 0 && playerTop >= 400){
+        obstacle.style.animation = "none";
+        obstacle.style.display = "none";
+        alert("You lost");
+    }
+},10);
+
 function HitDetected(){
     
 }
@@ -8,9 +19,10 @@ function HitDetected(){
 function JumpPressed(){
     if (player.classList != "animate"){
         player.classList.add("animate");
+        
+        setTimeout(function(){
+            player.classList.remove("animate");
+        },700);
     }
-    setTimeout(function(){
-        player.classList.remove("animate");
-    },700);
         
 }
