1759c0ca6ab77d890ddd34750b7e6779bf149bdf
DaleWillemse <DaleWillemse000@gmail.com>
2022-08-10 21:42:27 +0200

    Work on point counter.

--- script.js
+++ script.js
@@ -1,28 +1,31 @@
 var player = document.getElementById("player");
 var obstacle = document.getElementById("obstacle");
+var scoreCounter = document.getElementById("score");
+var score = 0;
 
-var obstacleHit = setInterval(function(){
+var obstacleHit = setInterval(function () {
     var playerTop = parseInt(window.getComputedStyle(player).getPropertyValue("top"));
     var obstacleLeft = parseInt(window.getComputedStyle(obstacle).getPropertyValue("left"));
 
-    if (obstacleLeft < 151 && obstacleLeft > 0 && playerTop >= 400){
+    if (obstacleLeft < 151 && obstacleLeft > 0 && playerTop >= 400) {
         obstacle.style.animation = "none";
         obstacle.style.display = "none";
         alert("You lost");
     }
-},10);
-
-function HitDetected(){
-    
-}
-
-function JumpPressed(){
-    if (player.classList != "animate"){
-        player.classList.add("animate");
-        
-        setTimeout(function(){
-            player.classList.remove("animate");
-        },700);
+    else{
+        score++;
+        scoreCounter.textContent(score)
     }
-        
-}
+}, 10);
+
+document.addEventListener("keypress", function (event) {
+    if (event.keyCode == 32) {
+        if (player.classList != "animate") {
+            player.classList.add("animate");
+
+            setTimeout(function () {
+                player.classList.remove("animate");
+            }, 700);
+        }
+    }
+});
