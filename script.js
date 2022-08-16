c34b5628d1d9b53c94587d6259e1eee570289c94
DaleWillemse <DaleWillemse000@gmail.com>
2022-08-16 20:52:45 +0200

    player jump working

--- script.js
+++ script.js
@@ -1,1 +1,13 @@
-////////////
+var player = document.getElementById("player");
+var obstacle = document.getElementById("obstacle");
+
+document.addEventListener("keypress", e => {
+    if (e.keyCode == 32) {
+        if (!player.classList.contains("animate")) {
+            player.classList.add("animate");
+            setTimeout(() => {
+                player.classList.remove("animate");
+            }, 900)
+        }
+    }
+});
