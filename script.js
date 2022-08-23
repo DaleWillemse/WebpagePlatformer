a10436b0bb5bc1322c35bd3c9ca27bac97e59e9e
DaleWillemse <DaleWillemse000@gmail.com>
2022-08-23 10:57:39 +0200

    +obstacle, +point counter. Fix : Jump.

--- script.js
+++ script.js
@@ -1,5 +1,8 @@
 var player = document.getElementById("player");
-var obstacle = document.getElementById("obstacle");
+var obstacleOne = document.getElementById("obstacleOne");
+var obstacleTwo = document.getElementById("obstacleTwo");
+var score = document.getElementById("score");
+var points = 0;
 
 document.addEventListener("keypress", e => {
     if (e.keyCode == 32) {
@@ -11,3 +14,46 @@ document.addEventListener("keypress", e => {
         }
     }
 });
+
+setInterval(() => {
+    let obstacleOnePos = obstacleOne.getBoundingClientRect();
+    let obstacleTwoPos = obstacleTwo.getBoundingClientRect();
+    let randomHeight = getRandomInt(5);
+
+    if (parseInt(obstacleOnePos.left) <= 0) {
+        switch (randomHeight) {
+            case 1:
+                obstacleOne.style.top = "250px";
+                break;
+            case 2:
+                obstacleOne.style.top = "345px";
+                break;
+            default:
+                obstacleOne.style.top = "445px;"
+                break;
+        }
+    }
+    if (parseInt(obstacleTwoPos.left) <= 0) {
+        switch (randomHeight) {
+            case 1:
+                obstacleTwo.style.top = "345px";
+                break;
+            case 2:
+                obstacleTwo.style.top = "250px";
+                break;
+            default:
+                obstacleTwo.style.top = "445px";
+                break;
+        }
+    }
+
+}, 100)
+
+setInterval(() => {
+    points++;
+    score.innerHTML = points;
+}, 1000)
+
+function getRandomInt(max) {
+    return (Math.floor(Math.random() * max));
+}
